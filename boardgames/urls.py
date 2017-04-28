14e8990741271e5453415d23eacd6a9ece4f0261
william chou <w.chou06@gmail.com>
2017-04-28 06:34:12 -0400

    Rendering styled view, routing works, starting model.

--- boardgames/urls.py
+++ boardgames/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from main import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'$', views.home, name='home')
+    url(r'^hello$', views.home, name='home') 
+    #r means raw string carrot means beginning, $ means end
 ]
--- main/views.py
+++ main/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("Hello World")
+  return render(request, "main/home.html", 
+  {'message': 'Hi there!'})
